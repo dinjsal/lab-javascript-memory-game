9187735e30fdd9f77626cd1a0832d54fc33ce7a6
Dinah Salazar <dinahsalazar@gmail.com>
2023-09-14 05:04:19 +0900

    Update iteration 3

--- src/index.js
+++ src/index.js
@@ -26,6 +26,9 @@ const cards = [
 ];
 
 const memoryGame = new MemoryGame(cards);
+const card = document.querySelectorAll('.card');
+
+
 
 window.addEventListener('load', (event) => {
   let html = '';
@@ -35,7 +38,7 @@ window.addEventListener('load', (event) => {
         <div class="back" name="${pic.img}"></div>
         <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
       </div>
-    `;
+    `;  
   });
 
   // Add all the divs to the HTML
@@ -44,8 +47,19 @@ window.addEventListener('load', (event) => {
   // Bind the click event of each element to a function
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
-      // TODO: write some code here
+        card.classList.toggle('back');
       console.log(`Card clicked: ${card}`);
     });
   });
 });
+
+checkIfFinished();
+
+
+//all selectors on top and then followed by the functions
+/*sequence:
+1. all cards faced down - loaded on the page already
+2. flipping movement - onclick
+3. if guess/same cards - onclick stay on faced up (with img)
+4. if x guessed - onclick, flip back to blue bg, faced down
+*/
